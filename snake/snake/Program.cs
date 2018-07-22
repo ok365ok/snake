d4599827fb1c73583221796d3515a695c5d66bcd
ok365ok <maxapachi@icloud.com>
2018-07-23 00:48:50 +0300

    the same one

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,17 +10,16 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Draw();
-
-            VerticalLine line2 = new VerticalLine(5, 10, 8, '+');
-            line2.Draw();
+            // отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+' );
+            HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+' );
+            VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+' );
+            VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+' );
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
             // отрисовка точек
             Point p = new Point(4, 5, '*');
